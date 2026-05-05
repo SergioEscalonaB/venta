2ad64b22b39cbf4218903819a520db647b07b20d
Sergio Luis <sergioluis2610@hotmail.com>
2026-05-05 16:36:12 -0500

    Correciones

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
       className="text-white d-flex align-items-center position-relative site-hero"
       style={{
         minHeight: "100vh",
-        backgroundImage: "url(/galeria/30.jpg)", // Cambiar aca la imgane principal de fondo
+        backgroundImage: "url(/galeria/14.jpg)", // Cambiar aca la imgane principal de fondo
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
